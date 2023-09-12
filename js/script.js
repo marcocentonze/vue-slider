ef1aab21fbbb393f9d60917fa19929edead1366e
marcocentonze <marco.centonze27@gmail.com>
2023-09-12 16:23:15 +0200

    add working arrows

--- js/script.js
+++ js/script.js
@@ -40,4 +40,21 @@ createApp({
       ],
     };
   },
+  methods: {
+    next() {
+      this.activeImage++;
+
+      if (this.activeImage > this.slides.length - 1) {
+        this.activeImage = 0;
+      }
+    },
+
+    prev() {
+      this.activeImage--;
+
+      if (this.activeImage < 0) {
+        this.activeImage = this.slides.length - 1;
+      }
+    },
+  },
 }).mount("#app");
